819b47ee5217eb8068938a42b7bac7675f1ff1aa
Bob's Cubicle <miraclemind.bobdev5@gmail.com>
2023-08-15 16:09:18 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require("mongoose");
 // require("./src/configs/db.config");
 
 const user_route = require('./src/routes/user.route');
+
+app.use(express.json);
 
 app.get('/', (req, res) => {
     res.send("Welcome to Astrocure App");
@@ -11,9 +14,17 @@ app.get('/', (req, res) => {
 
 app.use("/user", user_route);
 
-const connectDb = ()=>{
-    mongoose
-}
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    // .catch((error) => {
+    //     console.log(error);
+    // });
+
+// const connectDb = ()=>{
+//     mongoose
+// }
 
 app.listen(process.env.PORT, () => {
     console.log(`Astrocure app listening on port ${process.env.PORT}`)
